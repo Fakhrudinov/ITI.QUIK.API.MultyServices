472cbfd7df3b249f74998f6b957753b3157b8fde
Alexander F <asbuka@gmail.com>
2022-06-08 16:24:09 +0300

    add child health check

--- ITI.QUIK.API.MultyServices/ITI.QUIK.API.MultyServices/Controllers/HealthController.cs
+++ ITI.QUIK.API.MultyServices/ITI.QUIK.API.MultyServices/Controllers/HealthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 
 namespace ITI.QUIK.API.MultyServices.Controllers
 {
@@ -7,10 +8,48 @@ namespace ITI.QUIK.API.MultyServices.Controllers
     [ApiController]
     public class HealthController : ControllerBase
     {
-        [HttpGet("OK")]
+        [HttpGet("MyHealth")]
         public IActionResult IsOk()
         {
-            return Ok("Yes");
+            return Ok("Ok");
+        }
+
+        [HttpGet("IsChildHealthOk")]
+        public async Task<IActionResult> IsChildOk()
+        {
+            string _baseAddress = "http://172.16.6.11:8002/";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                //client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var responseMessage = await client.GetAsync(_baseAddress + "api/HealthState/OK");
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    string responseBody = await responseMessage.Content.ReadAsStringAsync();
+                    return Ok(responseBody);
+                }
+            }
+            return NotFound();
+
+
+            //HttpClient client = new HttpClient();
+
+            //client.BaseAddress = new Uri("http://172.16.6.11:8002/");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            //HttpResponseMessage response = await client.GetAsync(url.PathAndQuery);
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var product = await response.Content.ReadAsAsync<Product>();
+            //}
+
+            ////return Ok("Yes");
         }
     }
 }
