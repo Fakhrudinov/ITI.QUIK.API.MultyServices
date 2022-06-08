8c66fca451595a26c8eadee3bace86ffdc07ecbf
Alexander F <asbuka@gmail.com>
2022-06-08 15:56:06 +0300

    Add HealtController

---
+++ ITI.QUIK.API.MultyServices/ITI.QUIK.API.MultyServices/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.QUIK.API.MultyServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("OK")]
+        public IActionResult IsOk()
+        {
+            return Ok("Yes");
+        }
+    }
+}
